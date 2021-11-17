4f8713a7055e21f4e2ebeed103b39b7b32795ac7
PAG <artem.lobach022003@gmail.com>
2021-11-17 18:02:31 +0100

    start

--- src/main/java/com/example/serverutil/Service/ServerRepoService.java
+++ src/main/java/com/example/serverutil/Service/ServerRepoService.java
@@ -1,15 +1,14 @@
 package com.example.serverutil.Service;
 
 import com.example.serverutil.model.Server;
-import com.example.serverutil.repos.ServerRepo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
 import java.util.Collection;
 
 public interface ServerRepoService {
-    public void saveServer(Server server);
-    Server ping(String ip);
+    public Server saveServer(Server server);
+    Server ping(String ip) throws UnknownHostException, IOException;
     Collection<Server> list(int limit);
     Server get(Long id);
     Server update(Server server);
--- src/main/java/com/example/serverutil/Service/ServerServiceImpl.java
+++ src/main/java/com/example/serverutil/Service/ServerServiceImpl.java
@@ -1,42 +1,76 @@
 package com.example.serverutil.Service;
 
 import com.example.serverutil.model.Server;
+import com.example.serverutil.model.enums.Status;
 import com.example.serverutil.repos.ServerRepo;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.io.IOException;
+import java.net.InetAddress;
 import java.util.Collection;
+import java.util.Random;
+
+import static org.springframework.data.domain.PageRequest.*;
+
 @Service
 @RequiredArgsConstructor
+@Transactional
+@Slf4j
 public class ServerServiceImpl implements ServerRepoService{
     private final ServerRepo serverRepo;
-    @Override
-    public void saveServer(Server server) {
 
+    @Override
+    public Server saveServer(Server server) {
+        log.info("Saving new server:{}",server.getServerName());
+        server.setImageUrl(setSeverImageUrl());
+        return serverRepo.save(server);
     }
 
+
+
     @Override
-    public Server ping(String ip) {
-        return null;
+    public Server ping(String ip) throws IOException {
+        log.info("Pinging server at:{}",ip);
+        Server server=serverRepo.findByIp(ip);
+        InetAddress address=InetAddress.getByName(ip);
+        server.setStatus(address.isReachable(10000)? Status.SERVER_UP :Status.SERVER_DOWN);
+        serverRepo.save(server);
+        return server;
     }
 
     @Override
     public Collection<Server> list(int limit) {
-        return null;
+        log.info("Servers");
+        return serverRepo.findAll(of(0,limit)).toList();
     }
 
     @Override
     public Server get(Long id) {
-        return null;
+        log.info("Fetching:");
+        return serverRepo.getById(id);
     }
 
     @Override
     public Server update(Server server) {
-        return null;
+        log.info("Updating server:{}",server.getServerName());
+        return serverRepo.save(server);
     }
 
     @Override
     public Boolean delete(Long id) {
-        return null;
+        log.info("Deleting:{}",serverRepo.getById(id).getServerName());
+        serverRepo.deleteById(id);
+        return Boolean.TRUE;
+    }
+
+    private String setSeverImageUrl() {
+        String[] imageNames={"server1.png","server2.png","server3.png","server4.png"};
+
+        return ServletUriComponentsBuilder.fromCurrentContextPath().path("/server/image/"
+                +imageNames[new Random().nextInt(4)]).toUriString();
     }
 }
